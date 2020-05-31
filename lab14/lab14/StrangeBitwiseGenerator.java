a60629d777f4571805767b5c67c70fb36d09db7d
Yi Sun <yisun@Yis-MacBook-Pro.fios-router.home>
2020-05-31 13:56:11 -0400

    fix lab14 bug

--- lab14/lab14/StrangeBitwiseGenerator.java
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -5,6 +5,8 @@ import lab14lib.Generator;
 public class StrangeBitwiseGenerator implements Generator {
     private int period;
     private int state;
+    private int max;
+    private int min = 0;
 
     public StrangeBitwiseGenerator(int period) {
         this.state = 0;
@@ -14,6 +16,11 @@ public class StrangeBitwiseGenerator implements Generator {
     public double next() {
         state = (state + 1);
         int weirdState = state & (state >>> 3) % period;
+        max = max > Math.abs(weirdState) ? max : Math.abs(weirdState);
+        if (max != 0) {
+            double res = (double) weirdState / (double) max;
+            return res;
+        }
         return weirdState;
     }
 }
